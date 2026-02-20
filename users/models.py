9de77acd4f47257a7448d45c81fca514f70a9c6d
chrry03 <ocr925@dgu.ac.kr>
2026-02-20 17:37:44 +0900

    fix: users랑 map에 모델 틀 추가 후, 마이그레이션 다시 함

--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# 일단 작동만 되게 만드는 최소한의 유저 모델
+class User(AbstractUser):
+    # 나중에 여기에 color_code 같은 걸 추가할 예정입니다!
+    pass
